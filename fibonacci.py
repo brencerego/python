1896116503425afc7bed056113849c065f77eb6c
brencerego <brencerego18@gmail.com>
2025-03-12 14:24:54 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,7 @@
-num=10
-n1,n2=0,1
-print("Fibonacci Series.")
-print(n1)
-print(n2)
-for i in range(2,num):
-     n3=n1+n2
-     n1=n2
-     n2=n3
-     print(n3)
+def fibonacci_sequence(n):
+    sequence = [0,1]
+    for i in range(2,n):
+        sequence.append(sequence[i-1]+sequence[i-2])
+    return sequence
+print(fibonacci_sequence(10))
+   
